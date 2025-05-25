e3dc47b93da0e2d7d4619fa6a6538cf5ec6ba512
Mr-Aura <numair5515@gmail.com>
2025-05-25 17:06:43 +0530

    Delete OOPS directory

--- OOPS/default_constructor.py
+++
@@ -1,8 +0,0 @@
-class Employee:
-    name = "Harry Kane"
-    occ = "Developer"
-    def __init__(self): # default constructor
-        print(self.name,"is a",self.occ)
-a = Employee()
-#default constructor is a constructor which doesn't accept any argument from object and 
-# has only one argument , self in constructor.
--- OOPS/single_inheritance.py
+++
@@ -1,9 +0,0 @@
-class Alpha:
-    name = "Harry Kane"
-    sal = 900000
-class Beta(Alpha): # here we pass the class we are inheriting as argument.
-    occ = "Footballer"
-    def info(self):
-        print(self.name,"is",self.occ)
-a = Beta()
-a.info()
