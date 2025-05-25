1846839f36d94f85c64c4141fd0b59440fd6140d
Mr-Aura <numair5515@gmail.com>
2025-05-25 17:07:32 +0530

    Delete User Defined Functions directory

--- User Defined Functions/arbitrary_keyword_arguments.py
+++
@@ -1,6 +0,0 @@
-# If we do not know the number of keyword arguments that will be passed in function then
-# we add '**' before parameter name. This way the function will receive a dictionary of
-# arguments and can access items accordingly. 
-def hello(**a):
-    print(a["y"])
-hello(x=10,y=20)
