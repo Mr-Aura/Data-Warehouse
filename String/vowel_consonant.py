e8753f8ac76de34acc9678561891c7b7a848f016
Mr-Aura <numair5515@gmail.com>
2025-05-25 17:07:06 +0530

    Delete String directory

--- String/vowel_consonant.py
+++
@@ -1,9 +0,0 @@
-vowel = consonant = 0
-str = input("Enter a string to check vowel and consonant:")
-for i in range(0,len(str)):
-    if str[i]=='a' or str[i]=='e' or str[i]=='i' or str[i]=='o' or str[i]=='u' or str[i]=='A' or str[i]=='E' or str[i]=='I' or str[i]=='O' or str[i]=='U':
-        vowel = vowel + 1
-    else:
-        consonant = consonant + 1
-print("Number of vowel =",vowel)
-print("Number of consonant =",consonant)
