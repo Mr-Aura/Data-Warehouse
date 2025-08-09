e93fdde2c680aebbe6d97d5fd926caeb8399efbe
Numair Khan <numair5515@gmail.com>
2025-08-09 14:49:17 +0530

    Added a number generator function and removed print() inside the function.

--- git_demo.py
+++ git_demo.py
@@ -1,10 +1,19 @@
+def number_generator(number):
+    my_list = []
+    for i in range(number):
+        my_list.append(i)
+    return my_list
+
 def fibonacci_generator(number):
+    my_list = []
     a = 0
     b = 1
     for i in range(number):
-     print(a)
+     my_list.append(a)
      c = a+b
      a = b
      b = c
+    return my_list
      
-fibonacci_generator(5)
+print(fibonacci_generator(5))
+print(number_generator(5))
